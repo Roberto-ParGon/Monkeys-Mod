b11456e51a8c82c0cf04c2a959916d7d397a73bf
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-09-28 22:58:48 -0600

    MonkeysMod template

---
+++ src/main/java/com/blossom/monkeysmod/MonkeysModClient.java
@@ -0,0 +1,11 @@
+package com.blossom.monkeysmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MonkeysModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/blossom/monkeysmod/MonkeysModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.blossom.monkeysmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MonkeysModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
